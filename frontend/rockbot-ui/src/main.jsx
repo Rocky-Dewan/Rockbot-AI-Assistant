8f9fbd11dfea989449350b4842ca0a3ae5b0feaf
Rocky Dewan <dewanrocky250@gmail.com>
2025-10-31 02:04:03 +0600

    Update main.jsx

--- frontend/rockbot-ui/src/main.jsx
+++ frontend/rockbot-ui/src/main.jsx
@@ -1,13 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    {/* Root wrapper that reacts to dark mode */}
-    <div className="min-h-screen w-full bg-white text-slate-900 dark:bg-[#0b1121] dark:text-slate-100 transition-colors duration-300">
-      <App />
-    </div>
-  </React.StrictMode>,
-);
+createRoot(document.getElementById("root")!).render(<App />);
